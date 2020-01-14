4ccdd9b783b86787cb05762ff1b759256f7d8e45
gingerjx <55396985+gingerjx@users.noreply.github.com>
2020-01-14 21:04:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import solve
 import time
 
 t0 = time.time()
-img = cv.imread("mazes/maze2.png")
-if  img == None :
+img = cv.imread("mazes/maze3.jpg")
+if  img is None:
     print("Wrong path to file")
     exit(1)
 imX, imY = img.shape[0], img.shape[1]
@@ -20,18 +20,22 @@ threshold = cv.adaptiveThreshold(grayscale,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv
 contours, hierarchy = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)    #finding contours
 
 start, end = points.findPoints(contours, out.shape)             #returns coordinates and radius of start and end point
-contours = helpful.contoursReduction(contours)                  #Reduce useless contours
-
-conImg = np.ones((out.shape[0], out.shape[1]), dtype=np.uint8)*255              #creating white image
-conImg = helpful.drawWithoutPoints(conImg,contours,start,end)                   #drawing contours on image
-
-print("Solving....")
-path, size = solve.Bfs(conImg,(start[1],start[0]),(end[1],end[0]),start[2])     #finding path
-
-if path is not None:
-    helpful.drawPath(out,path,start,end,size)                                   #drawing path in out image
-    cv.imwrite("mazes\solvedMaze.jpg",out)
+if start is None:
+    print("Cannot find start and end points.")
 else:
-    print ("Queue has been exhausted. No answer was found.")
-    
-print("Finish after: ", time.time()-t0, "seconds")
+    contours = helpful.contoursReduction(contours)                  #Reduce useless contours
+
+    conImg = np.ones((out.shape[0], out.shape[1]), dtype=np.uint8)*255              #creating white image
+    conImg = helpful.drawWithoutPoints(conImg,contours,start,end)                   #drawing contours on image
+
+
+    print("Solving....")
+    path, size = solve.Bfs(conImg,(start[1],start[0]),(end[1],end[0]),start[2])     #finding path
+
+    if path is not None:
+        helpful.drawPath(out, path, start, end, size)                                   #drawing path in out image
+        cv.imwrite("/home/piotrek/PycharmProjects/MazeSolving/mazes/solvedMaze.jpg", out)
+    else:
+        print ("Cannot find escape path.")
+
+    print("Finish after: ", time.time()-t0, "seconds")
